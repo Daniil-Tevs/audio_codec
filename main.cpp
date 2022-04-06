67a550e51060ef57e3cd9dd1bce12a1cba2084e8
Daniil <daniil_tevs@mail.ru>
2022-04-06 18:53:29 +0200

    refactor .dat

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include "speex.h"
 #include "speex_bits.h"
 #include "speex_types.h"
-#include <vector>
 #include <string>
 #include <fstream>
 
@@ -41,7 +40,7 @@ void make_header_wav(const std::string& output_wav,int channels,int rate)//бе�
 //}
 
 int main() {
-    std::string name_input ="../Hello.enc", name_out = "../output.wav",tmp_file = "../output.txt";
+    std::string name_input ="../Hello.dat", name_out = "../output.wav";
     setlocale(LC_ALL,"Rus");
     std::ifstream inb(name_input,std::ios::binary);
     make_header_wav(name_out,1,8000);
@@ -53,115 +52,67 @@ int main() {
     speex_bits_init(&dec_bits);
     dec_state = speex_decoder_init(&speex_nb_mode);
 
-    int frame_size =20;
-    speex_decoder_ctl(dec_state, SPEEX_GET_FRAME_SIZE, &frame_size);
-
     char in_b[100];
-    int s=0,byte_t;
-    bool fl = true;
-    std::vector<char> t;
-    std::vector<spx_int16_t > out_i;
+    int s=0;
     spx_int16_t out[10000];
-    char byte;
 
-    if(name_input.find(".enc")) {
+    if(name_input.find(".enc")!=-1) {
+        char byte;
         while(!inb.eof()) {
             int second, first;
-            /*if (fl) { inb >> byte; }
-            else { fl = true; }*/
             inb.read(&byte, sizeof(byte));
-            second = (byte >> 4) & 0xF;
-            first = byte & 0xF;
-            inb.read(in_b, second);
-            inb.read(in_b + second, first);
-            /*for (int i = 0; i < second; i++) {
-                inb >> in_b[first + i];
-                if (int(in_b[first + i]) == 17 || int(in_b[first + i]) == 102 || int(in_b[first + i]) == 22 || int(in_b[first + i]) == 97) {
-                    byte = in_b[first + i];
-                    fl = false;
-                    break;
-                }
-            }*/
-            /*if(fl) {
-                for (int i = 0; i < first; i++) {
-                    inb >> in_b[i];
-                    if (int(in_b[i]) == 17 || int(in_b[i]) == 102 || int(in_b[i]) == 22 || int(in_b[i]) == 97) {
-                        byte = in_b[i];
-                        fl = false;
-                        break;
-                    }
-                }
-            }*/
-            std::cout << std::hex << int(second) << " " << int(first) << " ";
+            first = (byte >> 4) & 0xF;
+            second = byte & 0xF;
+            inb.read(in_b, first);
+            inb.read(in_b+first, second);
+            std::cout << std::hex << int(first) << " " << int(second) << " ";
             for (int i = 0; i < second + first; i++)
                 std::cout << std::hex << int(in_b[i]) << " ";
             std::cout << std::endl;
-            speex_bits_read_from(&dec_bits, in_b+second,first);
+            speex_bits_read_from(&dec_bits, in_b,first);
             speex_decode_int(dec_state, &dec_bits, out);
             out_f.write(reinterpret_cast<const char *>(&out), 160);
             speex_bits_reset(&dec_bits);
-            speex_bits_read_from(&dec_bits, in_b,second);
+            speex_bits_read_from(&dec_bits, in_b+first,second);
             speex_decode_int(dec_state, &dec_bits, out);
             out_f.write(reinterpret_cast<const char *>(&out), 160);
             speex_bits_reset(&dec_bits);
-            t.clear();
-            s+=320;
+            s++;
         }
     }
-    else if(name_input.find(".dat")) {
+    else if(name_input.find(".dat")!=-1) {
+        int byte;
+        int c;
         while(!inb.eof()) {
-            int b, c;
-            inb >> std::dec >> byte_t;
-            b = byte_t / 10;
-            c = byte_t % 10;
-            for (int i = 0; i < b + c; i++) {
-                int a;
-                inb >> std::hex >> a;
-                t.push_back(a);
+            int second, first;
+            inb>>std::dec>>byte;
+            first = byte%10;
+            second = byte / 10;
+            for(int i=0;i<first+second;i++)
+            {
+                inb>>std::hex>>c;
+                in_b[i] = c;
             }
-            for (int i = 0; i < b + c; i++) {
-                if (i < c)
-                    in_b[b + i] = t[i];
-                else
-                    in_b[i - c] = t[i];
-            }
-            std::cout << std::hex << int(b) << " " << int(c) << " ";
-            for (int i = 0; i < b + c; i++)
+            std::cout << std::hex << int(first) << " " << int(second) << " ";
+            for (int i = 0; i < second + first; i++)
                 std::cout << std::hex << int(in_b[i]) << " ";
             std::cout << std::endl;
-            speex_bits_read_from(&dec_bits, in_b, b + c);
+            speex_bits_read_from(&dec_bits, in_b,first);
             speex_decode_int(dec_state, &dec_bits, out);
+            out_f.write(reinterpret_cast<const char *>(&out), 160);
             speex_bits_reset(&dec_bits);
-            for (int i = 0; i < t.size() * 8; i++) {
-                out_f << std::hex << (int) out[i] << " ";
-                s++;
-            }
-            t.clear();
+            speex_bits_read_from(&dec_bits, in_b+first,second);
+            speex_decode_int(dec_state, &dec_bits, out);
+            out_f.write(reinterpret_cast<const char *>(&out), 160);
+            speex_bits_reset(&dec_bits);
+            s++;
         }
     }
     else
         std::cout<<"Error, program don't work with this extension file";
-    out_f.clear();
 
-//    int size_f=44+s;
-//    out_f.open(name_out,std::ios::ate);
-//    out_f.seekp(5);
-//
-////    out_f.write(reinterpret_cast<const char *>(size_f), 1);
-//    std::cout<<std::dec<<s<<std::endl;
-//    out_f.close();
-//    inb.close();
-//    make_header_wav(name_out,1,8000,s);
-//    std::ifstream in_e(tmp_file,std::ios::binary);
-//    std::ofstream out_e(name_out,std::ios::app);
-//    while(!in_e.eof())
-//    {
-//        char a;
-//        in_e>>std::hex>>a;
-//        out_e<<std::hex<<a;
-//    }
-//    std::cout<<std::dec<<s<<std::endl;
-    int size_data = s,size_file = size_data+44;
+    out_f.clear();
+    int size_data = s*320,size_file = size_data+44;
     out_f.close();
     out_f.flush();
     out_f.open(name_out,std::ios::binary | std::ios::in);
